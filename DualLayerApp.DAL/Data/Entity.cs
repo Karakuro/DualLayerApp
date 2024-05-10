7d64b7b15ebfacc38683faf2728741fc258f66cf
Domenico Sironi <domenico.sironi@reti.it>
2024-05-10 09:47:22 +0200

    Add project files.

---
+++ DualLayerApp.DAL/Data/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualLayerApp.DAL.Data
+{
+    public class Entity
+    {
+        public int EntityId { get; set; }
+        public string Name { get; set; }
+    }
+}
